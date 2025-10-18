1f803063135c7e2a89701f78e05c37502f5d80b8
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2025-10-18 04:08:47 -0300

    Test: Test deploy

--- src/app/contact/ButtonContact.tsx
+++ src/app/contact/ButtonContact.tsx
@@ -7,7 +7,7 @@ export default function ButtonContact(){
 
         <a href="https://wa.me/15551234567?text=Olá,%20Gostaria%20de%20saber%20mais%20sobre%20os%20seus%20seviços.">
             <button className='bg-[#ffffff] text-[#050603] w-full h-[50px] rounded-md mt-5 cursor-pointer'>
-                Enviar
+                Enviar mensagem
             </button>
         </a>
     )
