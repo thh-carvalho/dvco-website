266c05a00d8ab52de21336e408db8af126845bfb
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2025-10-18 04:14:39 -0300

    Fix: Fixing the wpp number on contact buttom

--- src/app/contact/ButtonContact.tsx
+++ src/app/contact/ButtonContact.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function ButtonContact(){
     return (
 
-        <a href="https://wa.me/15551234567?text=Olá,%20Gostaria%20de%20saber%20mais%20sobre%20os%20seus%20seviços.">
+        <a href="https://wa.me/5511989416460?text=Olá,%20Gostaria%20de%20saber%20mais%20sobre%20os%20seus%20seviços.">
             <button className='bg-[#ffffff] text-[#050603] w-full h-[50px] rounded-md mt-5 cursor-pointer'>
                 Enviar mensagem
             </button>
