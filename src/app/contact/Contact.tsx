e42ce785c287c37eff0fa02347eb34b2fab1c409
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2025-10-21 16:16:01 -0300

    feat: Put the anchor on contact buttom on hero section

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -5,7 +5,7 @@ import ButtonContact from './ButtonContact'
 
 export default function Contact(){
     return (
-        <section className="p-5">
+        <section id='contact-section' className="p-5">
             <h2 className='text-[#ffffff] pb-5 text-[20px] font-semibold'>Entre em contato</h2>
             <div className='flex flex-col gap-5'>
                 <p className='text-[#ffffff] text-[16px]'>Nos envie uma mensagem que logo um de nossos consultores entrará em contato.</p>
--- src/app/home/ButtonHome.tsx
+++ src/app/home/ButtonHome.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 
 export default function ButtomHome(){
     return (
-        <button className='bg-[#050603] text-[#ffffff] w-[150px] h-[50px] rounded-md mt-5 cursor-pointer'>Entrar em contato</button>
+        <a href="#contact-section">
+            <button className='bg-[#050603] text-[#ffffff] w-[150px] h-[50px] rounded-md mt-5 cursor-pointer'>Entrar em contato</button>
+        </a>
     )
 }
