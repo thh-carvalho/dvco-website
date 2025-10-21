11ec889a1f971e07a5051f13783201a6962e4c74
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2025-10-21 16:17:04 -0300

    update: deploy

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Use the correct basePath and assetPrefix for production builds
-  // basePath: `/dvco-website`,
-  // output: "export", // Enables static exports
-  // reactStrictMode: true,
+  basePath: `/dvco-website`,
+  output: "export", // Enables static exports
+  reactStrictMode: true,
 
-  // // This is the crucial part for images to work in a static export
-  // images: {
-  //   unoptimized: true,
-  // },
+  // This is the crucial part for images to work in a static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
